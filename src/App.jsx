1b94d54d29665925cb0dd0d0a07f33f494542a87
MusabSheikh1 <musabsheikh787@gmail.com>
2025-08-28 16:39:23 +0500

    Fix Home default export for Netlify build

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ import "./App.css";
 function App() {
   return (
     <Routes>
-      <Route path="/home" element={<Home />} />
+      <Route path="/" element={<Home />} />
       <Route path="/about" element={<About />} />
       <Route path="/" element={<Login />} />
       <Route path="/signup" element={<Signup />} />
